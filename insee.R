e5081d16ea8c57ef33d00b57b6626aea4a841b5c
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2023-10-09 11:40:42 +0200

    update

--- insee.R
+++ insee.R
@@ -12,18 +12,46 @@ save(insee_datasets, file = "insee_datasets.RData")
 
 View(insee_datasets)
 
-# insee_datastructure ------
+# insee_datastructure_2 ------
 
 insee_datastructure <- "https://www.bdm.insee.fr/series/sdmx/datastructure/FR1/IPLA-IPLNA-2015" %>%
   readSDMX()
 
-insee_datastructure_2 <- "https://www.bdm.insee.fr/series/sdmx/datastructure/FR1/IPLA-IPLNA-2015" %>%
+insee_datastructure <- "https://www.bdm.insee.fr/series/sdmx/datastructure/FR1/IPLA-IPLNA-2015" %>%
   readSDMX() %>%
   slot("datastructures") %>%
   pluck(1) %>%
   slot("Components") %>%
   as_tibble
 
-save(insee_datastructure_2, file = "insee_datastructure_2.RData")
+save(insee_datastructure, file = "insee_datastructure.RData")
 
-View(insee_datastructure_2)
+View(insee_datastructure)
+
+# insee_dataset ---------
+
+insee_dataset <- "https://bdm.insee.fr/series/sdmx/data/IPLA-IPLNA-2015" %>%
+  readSDMX %>%
+  as_tibble %>%
+  mutate(OBS_VALUE = as.numeric(OBS_VALUE))
+
+save(insee_dataset, file = "insee_dataset.RData")
+
+insee_dataset %>%
+  group_by(INDICATEUR) %>%
+  summarise(Nobs = n())
+
+# insee_codelist ----------
+
+insee_codelist <- paste0("https://www.bdm.insee.fr/series/sdmx/codelist/FR1/CL_INDICATEUR") %>%
+  readSDMX() %>%
+  as_tibble %>%
+  select(INDICATEUR = id, Indicateur = label.fr)
+
+
+insee_dataset %>%
+  left_join(insee_codelist, by = "INDICATEUR") %>%
+  group_by(INDICATEUR, Indicateur) %>%
+  summarise(Nobs = n())
+
+
