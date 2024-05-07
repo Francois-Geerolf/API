aed1a69e037a205e6c59684e61fb8692282cf5c9
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2024-05-07 16:19:37 +0200

    new version of OECD Data Explorer
    See for example:
    https://data-explorer.oecd.org/vis?tm=QNA&pg=0&snb=16&df[ds]=dsDisseminateFinalDMZ&df[id]=DSD_NAMAIN1%40DF_QNA_EXPENDITURE_CAPITA&df[ag]=OECD.SDD.NAD&df[vs]=1.0&pd=%2C&dq=Q..USA%2BEA20........LR..&ly[rw]=REF_AREA&ly[cl]=TIME_PERIOD&to[TIME_PERIOD]=false&lo=5&lom=LASTNPERIODS

--- oecd.R
+++ oecd.R
@@ -1,39 +1,62 @@
-# install.packages("rsdmx")
-library(rsdmx)
-library(tidyverse)
-library(scales)
-source("oecd_functions.R")
+library("tidyverse")
+library("rsdmx")
+library("zoo")
+library("scales")
 
-# oecd_codelist --------
+nber_recessions <- read_csv("
+Peak,Trough
+2001-03-01,2001-11-01
+2007-12-01,2009-06-01
+2020-02-01,2020-04-01
+")
 
-oecd_datasets <- "https://stats.oecd.org/RestSDMX/sdmx.ashx/GetKeyFamily/all" %>%
+cepr_recessions <- read_csv("
+Peak,Trough
+2019-10-01,2020-04-01
+2011-07-01,2013-01-01
+2008-01-01,2009-04-01
+")
+
+QNA_EXPENDITURE_CAPITA <- "https://sdmx.oecd.org/public/rest/data/OECD.SDD.NAD,DSD_NAMAIN1@DF_QNA_EXPENDITURE_CAPITA,1.0/Q..USA+EA20........LR.." %>%
   readSDMX() %>%
   as_tibble
 
-oecd_datastructure <- "http://stats.oecd.org/restsdmx/sdmx.ashx/GetDataStructure/QNA" %>%
-  readSDMX() %>%
-  slot("concepts") %>%
-  as.data.frame()
+QNA_EXPENDITURE_CAPITA_var <- "https://sdmx.oecd.org/public/rest/dataflow/OECD.SDD.NAD/DSD_NAMAIN1@DF_QNA_EXPENDITURE_CAPITA/1.0?references=all" %>%
+  readSDMX()
 
-oecd_codelist <- "http://stats.oecd.org/restsdmx/sdmx.ashx/GetDataStructure/QNA" %>%
-  readSDMX() %>%
-  slot("codelists") %>%
-  as.data.frame(codelistId = "CL_QNA_SUBJECT")
+metadata_load_fr <- function(code, CL_code, data = QNA_EXPENDITURE_CAPITA_var){
+  assign(code, as.data.frame(data@codelists, codelistId = CL_code) %>%
+           select(id, label.fr) %>%
+           setNames(c(code, str_to_title(code))),
+         envir = .GlobalEnv)
+}
 
-# Example ---------
+metadata_load_fr("REF_AREA", "CL_AREA")
 
-"http://stats.oecd.org/restsdmx/sdmx.ashx/GetData/QNA/EA20+USA.B1_GE.VOBARSA.Q?startTime=2000" %>%
-  readSDMX() %>%
-  as_tibble %>%
-  quarter_to_date %>%
-  select_if(~ n_distinct(.) > 1) %>%
-  group_by(LOCATION) %>%
-  mutate(obsValue = 100 * obsValue / obsValue[date == as.Date("2007-01-01")]) %>%
-  ggplot(.) + theme_minimal() + xlab("") + ylab("Real GDP (2007 = 100)") +
-  geom_line(aes(x = date, y = obsValue, color = LOCATION)) +
-  scale_color_manual(values = c("#003399", "#BF0A30")) +
-  scale_x_date(breaks = seq(1960, 2026, 2) %>% paste0("-01-01") %>% as.Date,
+QNA_EXPENDITURE_CAPITA %>%
+  mutate(date = obsTime %>% as.yearqtr(format = "%Y-Q%q") %>% as.Date()) %>%
+  filter(year(date) >= 1999) %>%
+  left_join(REF_AREA, by = "REF_AREA") %>%
+  group_by(Ref_area) %>%
+  arrange(date) %>%
+  mutate(obsValue = 100 * obsValue / obsValue[1]) %>%
+  ggplot(.) + theme_minimal() + xlab("") + ylab("PIB par habitant (1999T1 = 100)") +
+  geom_line(aes(x = date, y = obsValue, color = Ref_area)) + 
+  scale_color_manual(values = c("#B22234", "#003399")) +
+  geom_rect(data = nber_recessions %>%
+              filter(Peak > as.Date("1999-01-01")), 
+            aes(xmin = Peak, xmax = Trough, ymin = 0, ymax = +Inf), 
+            fill = '#B22234', alpha = 0.1)  +
+  geom_rect(data = cepr_recessions %>%
+              filter(Peak > as.Date("1999-01-01")), 
+            aes(xmin = Peak, xmax = Trough, ymin = 0, ymax = +Inf), 
+            fill = '#003399', alpha = 0.1)  +
+  scale_x_date(breaks = c(seq(1999, 2100, 5), seq(1997, 2100, 5)) %>% paste0("-01-01") %>% as.Date,
                labels = date_format("%Y")) +
-  scale_y_log10(breaks = seq(70, 200, 2))
+  theme(legend.position = c(0.26, 0.8),
+        legend.title = element_blank()) +
+  scale_y_log10(breaks = seq(50, 200, 5))
+
+
 
 
